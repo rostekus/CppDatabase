05a57427e074e61dd4430161411e5ae2234bd7b7
rostekus <rostyslavmosorov@gmail.com>
2023-07-10 14:06:58 +0200

    tests: changed tests framework to gtests

--- tests/include/tests.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef TESTS_H
-#define TESTS_H
-
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
-
-#endif  // TESTS_H
--- tests/src/basic-tests.cpp
+++
@@ -1,5 +0,0 @@
-#include "tests.hpp"
-
-TEST_CASE("tests") {
-  SECTION("test to pass") { REQUIRE(true); }
-}
