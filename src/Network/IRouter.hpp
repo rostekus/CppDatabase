fd7f3434fbdb39561869a41cebaa4d91b5ae5988
rostekus <rostyslavmosorov@gmail.com>
2023-07-16 22:01:55 +0200

    fix: declare methods in IRouter as pure virtual

--- src/Network/IRouter.hpp
+++ src/Network/IRouter.hpp
@@ -10,8 +10,8 @@ namespace httpserver {
 
 class IRouter {
  public:
-  virtual void registerRoute(std::string url, Method method, IHandler handler);
-  virtual void route();
+  virtual void registerRoute(std::string url, Method method, IHandler handler)=0;
+  virtual void route(std::string) =0;
 };
 }  // namespace httpserver
 #endif
