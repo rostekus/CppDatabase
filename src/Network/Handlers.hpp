74eb6a6d1c887300b68958ebdfc6602e28e919b9
rostekus <rostyslavmosorov@gmail.com>
2023-07-20 16:40:02 +0200

    feat!: added DbHandlers
    added abstact DBHandler and one handler for inserting values and keys to db

---
+++ src/Network/Handlers.hpp
@@ -0,0 +1,32 @@
+#ifndef HPP_HANDLERS
+#define HPP_HANDLERS
+
+#include <algorithm>
+#include <memory>
+
+#include "../Database/IDatabase.hpp"
+#include "API/IHandler.hpp"
+#include "Types/Request.hpp"
+#include "Types/Response.hpp"
+
+namespace httpserver {
+
+class DBHandler : public IHandler {
+ public:
+  DBHandler(std::shared_ptr<db::IDatabase> db) : database(std::move(db)) {}
+  std::shared_ptr<db::IDatabase> getDB();
+
+ private:
+  std::shared_ptr<db::IDatabase> database;
+};
+
+class InsertKeyValueHandler : public DBHandler {
+ public:
+  InsertKeyValueHandler(std::shared_ptr<db::IDatabase> db)
+      : DBHandler(std::move(db)){};
+  Response handle(Request req) override;
+};
+
+}  // namespace httpserver
+
+#endif  // !HPP_HANDLERS
