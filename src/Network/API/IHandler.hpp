9b8edf5c29aa0059815c63b970448ed8f9f38f40
rostekus <rostyslavmosorov@gmail.com>
2023-07-20 16:36:07 +0200

    fix: made IHandler method pure virtual

--- src/Network/API/IHandler.hpp
+++ src/Network/API/IHandler.hpp
@@ -1,4 +1,6 @@
 #ifndef HPP_IHANDLER
+
+
 #define HPP_IHANDLER
 
 #include "../Types/Request.hpp"
@@ -7,7 +9,7 @@
 namespace httpserver {
 class IHandler {
  public:
-  virtual Response handle(Request req);
+  	virtual Response handle(Request req)=0;
 };
 }  // namespace httpserver
 #endif  // HPP_IHANDLER
