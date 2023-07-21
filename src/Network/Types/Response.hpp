31da168c564f499138a87c3ee06b0586572a2442
rostekus <rostyslavmosorov@gmail.com>
2023-07-21 12:26:12 +0200

    restyle: restyled using clang

--- src/Network/Types/Response.hpp
+++ src/Network/Types/Response.hpp
@@ -8,7 +8,7 @@ typedef std::map<std::string, std::string> Headers;
 typedef std::map<std::string, std::string> Body;
 class Response {
  public:
-	Response() = default;
+  Response() = default;
   Response(int code, Headers headers, Body body)
       : code(code), mHeaders(headers), mBody(body) {}
   int code;
