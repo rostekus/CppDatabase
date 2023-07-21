eed89dd832fdc762f5b930f54da6e6a950bfe47a
rostekus <rostyslavmosorov@gmail.com>
2023-07-21 12:24:25 +0200

    feat: added typedef for URL
    typedef std::string URL

--- src/Network/Types/Request.hpp
+++ src/Network/Types/Request.hpp
@@ -11,14 +11,15 @@ namespace httpserver {
 enum Method { GET, POST, DELETE, PUT, PATCH };
 typedef std::map<std::string, std::string> Headers;
 typedef std::map<std::string, std::string> BodyJson;
+typedef std::string Url;
 
 class Request {
  public:
-  std::string url;
+  Url url;
   Method method;
   Headers headers;
   BodyJson body_json;
-  Request(Method method, std::string url, Headers headers, BodyJson body)
+  Request(Method method, Url url, Headers headers, BodyJson body)
       : method(method), headers(headers), body_json(body), url(url){};
 };
 }  // namespace httpserver
