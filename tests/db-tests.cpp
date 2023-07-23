a9ed21f7b7ab1081a7c9b399de928fbecb4ef128
rostekus <rostyslavmosorov@gmail.com>
2023-07-23 20:46:07 +0200

    test: added tests for request parser

--- tests/db-tests.cpp
+++ tests/db-tests.cpp
@@ -1,9 +1,18 @@
 #include "Database/ExtDatabase.hpp"
 #include "gtest/gtest.h"
 
-TEST(ExtDatabase, insert) {
-  extdb::ExtDatabase db("");
-  db.setKeyValue("key", "value");
-  EXPECT_EQ("value", db.getValueKey("key"));
+using namespace extdb;
+TEST(ExtDatabaseTest, SetGetValueKeyTest) {
+  std::string dbName = "test_db";
+  std::string key = "test_key";
+  std::string value = "test_value";
+
+  ExtDatabase db(dbName);
+
+  db.setKeyValue(key, value);
+
+  std::string retrievedValue = db.getValueKey(key);
+
+  EXPECT_EQ(retrievedValue, value);
   db.destroy();
 }
